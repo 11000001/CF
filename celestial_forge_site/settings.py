0cd0d3b4bfa3eee364a4f18ec2fd702b354fbe05
rab <r.ahmedbuehler@gmail.com>
2021-02-24 15:18:31 -0800

    Fix syntax error (STATICFILES)

--- celestial_forge_site/settings.py
+++ celestial_forge_site/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 
